ee407b7bfdfcc9dc1eda526b5ebd83f27c58fb91
GuilhermeMarques1 <g-marques@outlook.com>
2024-03-10 10:19:47 -0300

    dev: Config typescript

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,12 @@
 import express, { Express, Request, Response } from "express";
 
 const app: Express = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
+  res.send("Hello, world!");
 });
 
 app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
