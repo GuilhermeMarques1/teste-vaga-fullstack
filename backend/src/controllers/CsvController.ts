71f964100efa03485705b84291cb7df9aafe6bfe
GuilhermeMarques1 <g-marques@outlook.com>
2024-03-11 03:51:10 -0300

    fix: Paginate all rows

--- backend/src/controllers/CsvController.ts
+++ backend/src/controllers/CsvController.ts
@@ -7,14 +7,29 @@ import { FormatData } from "../Helpers/FormatCsv";
 import { ValidateCpfCnpj } from "../Middleware/ValidateCpfCnpj";
 
 const csvPath = path.join(__dirname, '../../../data.csv');
-const pageSize = 1000;
+const pageSize = 10;
+
+function countLines(filePath: string): Promise<number> {
+  return new Promise((resolve, reject) => {
+    let totalLines = 0;
+    fs.createReadStream(filePath)
+      .pipe(csvParser())
+      .on('data', (row: ICsvRow) => {
+        if(ValidateCpfCnpj(row.nrCpfCnpj)) totalLines++;
+      })
+      .on('end', () => resolve(totalLines))
+      .on('error', (error) => reject(error));
+  });
+}
 
 export async function getData(req: Request, res: Response) {
   const page = Number(req.query.page) || 1;
   const startRow = (page - 1) * pageSize;
 
   const data: ICsvRow[] = [];
-  let currentRow = 0;
+
+  const totalLines = await countLines(csvPath);
+  const totalPages = Math.ceil(totalLines/pageSize)
 
   const stream = fs.createReadStream(csvPath)
     .pipe(csvParser())
@@ -27,22 +42,30 @@ export async function getData(req: Request, res: Response) {
       });
     });
   
+  let countData = 0;
   stream.on('data', (row: ICsvRow) => {
-    if (
-        currentRow >= startRow && 
-        currentRow < startRow + pageSize &&
-        ValidateCpfCnpj(row.nrCpfCnpj)
-      ) 
-    {
+    if(startRow == 0 && ValidateCpfCnpj(row.nrCpfCnpj)) {
       const formattedRow = FormatData(row);
-
+  
       data.push(formattedRow);
+    } else {
+      if(ValidateCpfCnpj(row.nrCpfCnpj)) {
+        countData++;
+        if(countData > pageSize*(page-1)) {
+          const formattedRow = FormatData(row);
+    
+          data.push(formattedRow);
+        }
+      }
     }
 
-    if(data.length === startRow + pageSize) stream.destroy();
+    if(data.length === pageSize) stream.destroy();
   });
 
   stream.on('close', () => {
-    res.status(200).json(data);
+    res.status(200).json({
+      data,
+      totalPages
+    });
   })
 }
