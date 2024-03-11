ae74e28bf2be21e4f5ac22500cfac25b1d4d7ccf
GuilhermeMarques1 <g-marques@outlook.com>
2024-03-11 01:22:36 -0300

    dev: Add and config toast

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,10 +1,13 @@
 import { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "@/styles/theme";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider resetCSS theme={theme}>
+      <ToastContainer autoClose={3000} />
       <Component {...pageProps} />
     </ChakraProvider>
   );
