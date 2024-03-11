4e5794035409cb52da3481793748adcdd7846dc4
GuilhermeMarques1 <g-marques@outlook.com>
2024-03-11 01:23:29 -0300

    feat: Request server data

--- frontend/src/Components/Table.tsx
+++ frontend/src/Components/Table.tsx
@@ -1,11 +1,35 @@
+import { useEffect, useState } from "react";
 import { 
   TableContainer, 
   Table as TableChakra,
   Thead, Tr, Th,
   Tbody, Td
 } from "@chakra-ui/react";
+import api from "@/services/api";
+import { ICsvRow } from "@/types/IData";
+import { toast } from "react-toastify";
 
 export function Table() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [data, setData] = useState<ICsvRow[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await api.get(`/data?page=${page}`);
+        console.log(res.data);
+        const data = res.data as ICsvRow[];
+        setData(data);
+        setIsLoading(false);
+      } catch (error) {
+        toast.error("Houve um erro ao buscar os dados");
+      }
+    }
+
+    if(isLoading) fetchData();
+  }, []);
+
   return (
     <TableContainer
       marginRight="4"
@@ -31,28 +55,21 @@ export function Table() {
           </Tr>
         </Thead>
         <Tbody>
-          <Tr>
-            <Td>41854274761</Td>
-            <Td>CLIENTE 1</Td>
-            <Td>533</Td>
-            <Td>27/12/2022</Td>
-            <Td>06/04/2022</Td>
-            <Td>83720.19</Td>
-            <Td>17524.03</Td>
-            <Td>536.4</Td>
-            <Td>47257.39</Td>
-          </Tr>
-          <Tr>
-            <Td>41854274761</Td>
-            <Td>CLIENTE 2</Td>
-            <Td>1150</Td>
-            <Td>27/12/2022</Td>
-            <Td>06/04/2022</Td>
-            <Td>83720.19</Td>
-            <Td>17524.03</Td>
-            <Td>536.4</Td>
-            <Td>47257.39</Td>
-          </Tr>
+          {
+            data.map((row) => (
+              <Tr>
+                <Td>{row.nrCpfCnpj}</Td>
+                <Td>{row.nmClient}</Td>
+                <Td>{row.nrInst}</Td>
+                <Td>{row.dtContrato}</Td>
+                <Td>{row.dtVctPre}</Td>
+                <Td>{row.vlTotal}</Td>
+                <Td>{row.vlPresta}</Td>
+                <Td>{row.vlMulta}</Td>
+                <Td>{row.vlAtual}</Td>
+              </Tr>
+            ))
+          }
         </Tbody>
       </TableChakra>
     </TableContainer>
